116a8ab13302c57bebe2d70ff86d1da6f74a37cd
Monhabell <167910199+Monhabell@users.noreply.github.com>
2025-02-18 18:43:53 -0500

    banners_ colocacion de banners y ajustes del cliente

--- src/components/shared/Header/Header.tsx
+++ src/components/shared/Header/Header.tsx
@@ -1,13 +1,15 @@
 'use client'
 import Link from 'next/link';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { faBars, faUser, faShoppingCart, faSearch } from '@fortawesome/free-solid-svg-icons';
 import { useState } from 'react';
+import Image from 'next/image';
 import './Header.sass';
 
 export const Header = () => {
     const [showMenu, setShowMenu] = useState(false);
-    
+    const [cartItems, setCartItems] = useState(2);
+
     const handleMenuToggle = () => {
         setShowMenu(!showMenu);
     }
@@ -17,11 +19,29 @@ export const Header = () => {
             <div className='main-header'>
                 <div className="logo-container">
                     <Link href="/">
-                        <h1>Dormi Luna</h1>
+                        <Image
+                            src="/images/logo.png" // Reemplaza con la ruta de tu imagen
+                            alt="Dormi Luna Logo"
+                            width={150} // Ajusta el tamaño según necesites
+                            height={50}
+                        />
                     </Link>
                 </div>
-                
+
+
+
                 <div className="nav-menu">
+                    {/* Usuario */}
+                    <Link href="/profile" className="icon-button">
+                        <FontAwesomeIcon icon={faUser} />
+                    </Link>
+
+                    {/* Carrito de Compras */}
+                    <Link href="/cart" className="icon-button">
+                        <FontAwesomeIcon icon={faShoppingCart} />
+                        {cartItems > 0 && <span className="cart-count">{cartItems}</span>}
+                    </Link>
+
                     <div className="dropdown-nav">
                         <button className='btn-primary' onClick={handleMenuToggle}>
                             <FontAwesomeIcon icon={faBars} size="lg" />
@@ -31,13 +51,13 @@ export const Header = () => {
                     <nav className={`${showMenu && 'show'}`}>
                         <ul>
                             <li className='nav-item'>
-                            <Link href="/store" onClick={handleMenuToggle}>Catálogo</Link>
+                                <Link href="/store" onClick={handleMenuToggle}>Catálogo</Link>
                             </li>
                             <li className='nav-item'>
-                            <Link href="/contact">Contáctanos</Link>
+                                <Link href="/contact">Contáctanos</Link>
                             </li>
                             <li className='nav-item'>
-                            <Link href="/contact">Nosotros</Link>
+                                <Link href="/contact">Nosotros</Link>
                             </li>
                         </ul>
                     </nav>
